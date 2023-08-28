adce8236213acf7aacd3350fc680afe5690a6c3b
irfankacapor <irfan.kacapor@outlook.com>
2023-08-28 23:39:50 +0200

    refactor: clean up code and use safer method for autocomplete on search

--- src/components/AutocompleteSearch.tsx
+++ src/components/AutocompleteSearch.tsx
@@ -1,82 +1,17 @@
 import { Filters, ProductCardProps } from "@/features/products-page/types";
 import useProducts from "@/hooks/useProducts";
 import React, { Dispatch, SetStateAction, useState } from "react";
-import styled from "styled-components";
 import SearchRoundedIcon from "@mui/icons-material/SearchRounded";
 import { colours } from "@/utils/colours";
-import { FONT_FAMILY } from "@/utils/global-styles";
 import { Divider } from "@mui/material";
-
-const SearchBarContainer = styled.div`
-  box-sizing: border-box !important;
-  position: relative;
-  width: 100% !important;
-`;
-
-const SearchIconContainer = styled.div`
-  position: absolute;
-  left: 20px;
-  top: 15px;
-  color: ${colours.grey};
-`;
-
-const StyledInput = styled.input`
-  width: calc(100% - 4rem);
-  height: 3rem;
-  border-radius: 2rem;
-  padding-left: 4rem;
-  font-size: 1rem;
-  font-family: ${FONT_FAMILY};
-  border: 1px solid ${colours.grey};
-
-  &:focus {
-    border: 1px solid ${colours.blue};
-  }
-  &:hover {
-    border: 1px solid ${colours.blue};
-  }
-  &:focus-visible {
-    border: 1px solid ${colours.blue} !important;
-    outline: none !important;
-  }
-`;
-
-const SearchButton = styled(({ ...props }) => <button {...props} />)`
-  position: absolute;
-  height: 3rem;
-  top: 2px;
-  right: -1px;
-  border-radius: 2rem;
-  background-color: ${colours.blue};
-  color: ${colours.white};
-  min-width: 100px;
-  border: none;
-  font-size: 1rem;
-  font-weight: 300;
-  font-family: ${FONT_FAMILY};
-  cursor: pointer;
-`;
-
-const Dropdown = styled.div`
-  overflow: hidden;
-  position: absolute;
-  border-radius: 1rem;
-  background-color: ${colours.white};
-  min-width: 160px;
-  width: calc(100% - 2rem);
-  box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
-  z-index: 1;
-`;
-
-const DropdownItem = styled.div`
-  color: black;
-  padding: 12px 16px;
-  text-decoration: none;
-  display: block;
-  &:hover {
-    background-color: ${colours.hoverhighlight};
-  }
-`;
+import {
+  Dropdown,
+  DropdownItem,
+  SearchBarContainer,
+  SearchButton,
+  SearchIconContainer,
+  StyledInput,
+} from "@/utils/autocomplete-search-styles";
 
 const AutocompleteSearch = ({
   setFilters,
@@ -111,8 +46,7 @@ const AutocompleteSearch = ({
 
   const handleKeyDown = (event: React.KeyboardEvent) => {
     if (event.key === "Enter") {
-      const itemTitle = document.getElementById(`item-${highlightedOption}`)
-        ?.innerText;
+      const itemTitle = filteredProducts[highlightedOption].title;
       setFilters((prevFilters) => ({
         ...prevFilters,
         searchedName:

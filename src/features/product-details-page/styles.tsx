06f61db9f9ac8afba91fb8ca000477e1420d56c0
irfankacapor <irfan.kacapor@outlook.com>
2023-09-04 10:36:36 +0200

    refactor: move the quick cart button to styles

--- src/features/product-details-page/styles.tsx
+++ src/features/product-details-page/styles.tsx
@@ -1,5 +1,5 @@
 import { colours } from "@/utils/colours";
-import { Button } from "@mui/material";
+import { Button, Link } from "@mui/material";
 import styled from "styled-components";
 
 export const AddToCartButton = styled(({ ...props }) => (
@@ -13,5 +13,25 @@ export const AddToCartButton = styled(({ ...props }) => (
   font-weight: 300 !important;
   :hover {
     box-shadow: rgba(140, 152, 164, 0.176) 0px 10px 40px 10px !important;
+  }
+`;
+
+export const QuickCartButton = styled(({ ...props }) => (
+  <Button variant="text" fullWidth size="large" component={Link} {...props} />
+))`
+  text-transform: none !important;
+  display: flex;
+  font-weight: 300 !important;
+  text-align: center;
+  border-radius: 5px !important;
+  padding: 10px 11px !important;
+  color: ${colours.title} !important;
+  width: 100%;
+  margin-top: 0.5rem !important;
+  & span {
+    & svg {
+      width: 20px;
+      height: 20px;
+    }
   }
 `;
